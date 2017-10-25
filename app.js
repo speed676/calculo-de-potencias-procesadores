31278a276caaf9c77e54f38c16842d1ba2dbb3f7
Victor Guillem Agustí <vidasoft.app@gmail.com>
2017-10-25 17:17:00 +0200

    Haciendo pruebas

--- app.js
+++ app.js
@@ -5,8 +5,31 @@ console.log("Parametros ----> "+params);
 
 // TODO comprobar
 
-let archivo = fs.readFileSync(params[2], 'utf-8');
-console.log(archivo);
+// let archivo = fs.readFileSync(params[2], 'utf-8');
+// console.log(archivo);
+
+fs.readFile(params[2], 'utf-8', (err, data) => {
+	if(err) {
+	  	console.log('error: ', err);
+	} else {
+		let result = data.toString().split('\n')
+		let par = 0;
+		for (var i in result) {
+			if(par)
+				console.log(result[i]);
+			else
+				console.log("Guardo valor: "+result[i])
+				datos[i] = result[i];
+		}
+		console.log("@@@@@@@@@@@@@@@@@@@@@@@@");
+		printFinal();
+	}
+});
+
+function printFinal() {
+	for(let i in datos)
+		console.log(datos[i])	
+}
 
 let datos = {
 	cores:"",
